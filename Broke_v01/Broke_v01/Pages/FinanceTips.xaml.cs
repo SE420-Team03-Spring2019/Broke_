230c8ddaf559467171c99e3445694ad1dce4bc56
NedahIbrahim <ibrahin3@my.erau.edu>
2019-04-10 11:20:53 -0700

    Made a funcion to randomize tips.

--- Broke_v01/Broke_v01/Pages/FinanceTips.xaml.cs
+++ Broke_v01/Broke_v01/Pages/FinanceTips.xaml.cs
@@ -23,12 +23,15 @@ namespace Broke_v01.Pages
         }
         private async void TipButt_Clicked(object sender, EventArgs e)
         {
-            List<string> mylist = new List<string>(new string[] { "Stop spending money you don't have", "Have a savings plan", "Have a will" });
-            await DisplayAlert("Here's a Tip", $" {mylist[0]}", "okay");
+
+            List<string> mylist = new List<string>(new string[] { "Stop spending money you don't have", "Have a savings plan", "Have a will" , "Don't be a dumbass", "Make your meals at home", "Stick to a budget", "Pay off your credit card debt", "Have a savings plan","Have a savings plan", "Have a no spend day"});
+            await DisplayAlert("Here's a Tip", $" {mylist[randomTip()]}", "okay");
         }
         public int randomTip()
         {
-            return;
+            Random random = new Random();
+            int num = random.Next(10);
+            return num;
         }
 
     }
