33465579f019a07208a865d206ebfdb2e8b7409b
NedahIbrahim <ibrahin3@my.erau.edu>
2019-04-11 09:22:43 -0700

    Added more finance tips and comments to all files

--- Broke_v01/Broke_v01/Pages/FinanceTips.xaml.cs
+++ Broke_v01/Broke_v01/Pages/FinanceTips.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*********************************************
+ * This file handles the finance tips page   *
+ * When the button is clicked it generates   *
+ * a random finacial tip from a list of tips.*
+ * *****************************************/
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +13,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
+// Create the page class.
 namespace Broke_v01.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -16,21 +23,32 @@ namespace Broke_v01.Pages
         {
             InitializeComponent();
         }
+        // Make the button and recognize the click.
         protected override void OnAppearing()
         {
-            TipButt.Clicked += TipButt_Clicked;
+            TipButton.Clicked += TipButton_Clicked;
 
         }
-        private async void TipButt_Clicked(object sender, EventArgs e)
+        // What to do when a button is clicked.
+        private async void TipButton_Clicked(object sender, EventArgs e)
         {
-
-            List<string> mylist = new List<string>(new string[] { "Stop spending money you don't have", "Have a savings plan", "Have a will" , "Don't be a dumbass", "Make your meals at home", "Stick to a budget", "Pay off your credit card debt", "Have a savings plan","Have a savings plan", "Have a no spend day"});
-            await DisplayAlert("Here's a Tip", $" {mylist[randomTip()]}", "okay");
+            // A list of strings that have the tips.
+            List<string> mylist = new List<string>(new string[] { "Stop spending money you don't have"
+            , "Have a savings plan", "Have a will" , "Don't be dumb and save your money", "Make your meals " +
+            "at home", "Stick to a budget", "Pay off your credit card debt", "Have a savings plan","Have a " +
+            "savings plan", "Have a no spend day", "List out your debt", "Pay more than the minimum on your" +
+            "credit card","Calculate your daily interest", "Automate your savings", " Do an annual review", 
+            "Write down your goals", "Create a file for taxes", "Plan fast meals", "Up your retirement contribution",
+            "Use cash in certain budget categories","Update your beneficiaries", "Check your credit report", 
+            "Calculate your net worth", "Avoid Student Loans (When Possible)", "Spend Less Than You Earn"});
+            // Display a message with a random tip.
+            await DisplayAlert("Here's a Tip", $" {mylist[RandomTip()]}", "okay");
         }
+        // Function that chooses a random number from 10 possible choices.
         public int RandomTip()
         {
             Random random = new Random();
-            int num = random.Next(10);
+            int num = random.Next(25);
             return num;
         }
 
--- Broke_v01/Broke_v01/Tip.cs
+++ Broke_v01/Broke_v01/Tip.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*********************************************
+ * This file handles the finance tips display* 
+ * feature.                                  *
+ * *****************************************/
+
+
+using System;
 namespace Broke_v01
 {
     public interface Tip
